b6c93138ac01c3d03864f43393702c7ebbc7290b
Josh Gearheart <other.josh.gearheart@gmail.com>
2020-02-16 06:59:54 -0800

    create react template

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Soon to become a functional React App.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Template.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Template.scss';
+
+function App() {
+  return (
+    <React.Fragment className="Template">
+      
+    </React.Fragment>
+  );
+}
+
+export default Template;
